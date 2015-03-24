7f11e0fb91990ff882b2e57324613268b4596bc3
zilon <a@vectordrop.com>
2015-03-25 01:23:40 +0200

    move global locale settings to application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ module Learn
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    I18n.available_locales = [:en, :ru]
+
+    def set_locale
+     I18n.locale = params[:locale] || I18n.default_locale
+     Rails.application.routes.default_url_options[:locale]= I18n.locale
+    end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
