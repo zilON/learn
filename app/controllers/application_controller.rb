2e4e0a8a9316529248e55645dcc9782e6b7bf846
zilon <a@vectordrop.com>
2015-03-25 01:28:38 +0200

    houndci  fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #before_action :set_locale
+  # before_action :set_locale
   protect_from_forgery with: :exception
-
-  private
-
   #def set_locale
   #  I18n.locale = params[:locale] || I18n.default_locale
   #  Rails.application.routes.default_url_options[:locale]= I18n.locale
