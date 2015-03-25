bcc52d82a5e1655fecc76732a3b225999fb2f6a6
zilon <a@vectordrop.com>
2015-03-25 12:57:37 +0200

    Fixes db colum names

--- db/migrate/20150324092244_create_cards.rb
+++ db/migrate/20150324092244_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.text :original
-      t.text :translated
-      t.date :review
+      t.text :original_text
+      t.text :translated_text
+      t.datetime :review_date
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,11 @@ ActiveRecord::Schema.define(version: 20150324092244) do
   enable_extension "plpgsql"
 
   create_table "cards", force: :cascade do |t|
-    t.text     "original"
-    t.text     "translated"
-    t.date     "review"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "original_text"
+    t.text     "translated_text"
+    t.datetime "review_date"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
