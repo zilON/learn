a7abb87ecb89f9693b3d9421f1bfa4a0fe0c1889
zilon <a@vectordrop.com>
2015-03-26 00:19:09 +0200

    new card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,50 @@
 class CardsController < ApplicationController
   def index
-    @cards = Card.all
+    @cards = Card.all.order("created_at DESC")
   end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to  action: 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'update'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+
+    @card.destroy
+    redirect_to  action: 'index'
+  end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
+
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ActiveRecord::Base
+  validates :original_text,
+    presence: true,
+    length: { minimum: 5 }
 end
