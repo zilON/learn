0a99a92d44141b0d41a91f0aea5ba954f29845c4
zilon <a@vectordrop.com>
2015-03-28 11:53:20 +0200

    seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'nokogiri'
+require 'open-uri'
+
+Card.destroy_all
+
+page = Nokogiri::HTML(open ("http://www.languagedaily.com/learn-german/vocabulary/common-german-words/"))
+
+page.css('.jsn-article-content > table > tbody > tr').each do |row|
+  de = row.css('td:nth-child(2)')
+  en = row.css('td:nth-child(3)')
+  Card.create(original_text: de.text, translated_text: en.text)
+end
+puts "Loading data complite"
