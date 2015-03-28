00065ff6bbee34c4047bf1b7354aebbaf17407fd
zilon <a@vectordrop.com>
2015-03-28 11:59:18 +0200

    add aditional data page

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,34 @@ page.css('.jsn-article-content > table > tbody > tr').each do |row|
   en = row.css('td:nth-child(3)')
   Card.create(original_text: de.text, translated_text: en.text)
 end
-puts "Loading data complite"
+puts "Loading data from 1 page complite"
+
+page = Nokogiri::HTML(open ("http://www.languagedaily.com/learn-german/vocabulary/most-common-german-words-2"))
+
+page.css('.jsn-article-content > table > tbody > tr').each do |row|
+  de = row.css('td:nth-child(2)')
+  en = row.css('td:nth-child(3)')
+  Card.create(original_text: de.text, translated_text: en.text)
+end
+puts "Loading data from 2 page complite"
+
+page = Nokogiri::HTML(open ("http://www.languagedaily.com/learn-german/vocabulary/common-german-words-3"))
+
+page.css('.jsn-article-content > table > tbody > tr').each do |row|
+  de = row.css('td:nth-child(2)')
+  en = row.css('td:nth-child(3)')
+  Card.create(original_text: de.text, translated_text: en.text)
+end
+puts "Loading data from 3 page complite"
+
+page = Nokogiri::HTML(open ("http://www.languagedaily.com/learn-german/vocabulary/common-german-words-4"))
+
+page.css('.jsn-article-content > table > tbody > tr').each do |row|
+  de = row.css('td:nth-child(2)')
+  en = row.css('td:nth-child(3)')
+  Card.create(original_text: de.text, translated_text: en.text)
+end
+
+puts "Loading data from 4 page complite"
+
+puts "All done"
