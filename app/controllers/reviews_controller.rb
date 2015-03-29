c829a83aa864442e5b8de9be4c19a50386eb1bb6
zilon <a@vectordrop.com>
2015-03-29 19:29:50 +0300

    2 type flash type mesages ––wip

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,18 +2,13 @@ class ReviewsController < ApplicationController
   before_filter :set_card, only: [:update, :show]
 
   def update
-    flash[:error] = nil
-    respond_to do |format|
-      if @card.check_translation(card_params[:original_text])
-        format.html { redirect_to @card, notice: That is rigtht! }
-
-        flash[:notice] = 'That is rigtht!'
-        @card.update_review_date!
-        redirect_to root_path
-      else
-        flash[:error] = 'Invalid translation, try again!'
-        render :show
-      end
+    if @card.check_translation(card_params[:original_text])
+      flash[:notice] = 'That is rigtht!'
+      @card.update_review_date!
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Invalid translation, try again!'
+      render :show
     end
   end
 
