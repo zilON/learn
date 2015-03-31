cdb2ccbf58154ddc978f4fcab0fca9ac85cf249f
zilon <a@vectordrop.com>
2015-03-31 17:12:49 +0300

    rewrite bad spec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe Card, type: :model do
+describe Card do
+  card = Card.create(original_text: "hausE SOmE ДоМ", translated_text: "hauses")
+  card_bad = Card.new(original_text: "home", translated_text: "home", review_date: Date.today)
+
   it 'check translation' do
-    card = Card.new(original_text: "дом")
     expect(card.check_translation("вигвам")).to be false
   end
 
-  it 'should check correct translation with strange case' do
-    card = Card.new(original_text: "Дом На ХоЛме", translated_text: "hause")
-    expect(card.check_translation("дОм на ХолмЕ")).to be true
+  it 'set review_date in fields with the same values not work' do
+    expect(card_bad.update_review_date!).to be false
   end
 
-  it 'set review_date in fields with the same values not work' do
-    card = Card.new(original_text: "home", translated_text: "home", review_date: Date.today)
-    expect(card.update_review_date!).to be false
+  it 'should check correct translation with strange case' do
+    expect(card.check_translation("hausE SOmE ДоМ")).to be true
   end
 
   it 'should set corrected review_date' do
-    card = Card.new(original_text: "hause", translated_text: "hauses", review_date: Date.today)
     expect(card.update_review_date!).to be true
   end
 
   it 'should have valid text' do
-    expect(Card.new(original_text: "Собака", translated_text: "Dog", review_date: Date.today)).to be_valid
+    expect(card).to be_valid
   end
 
   it 'incorrect value in fields not saved' do
-    expect(Card.new(original_text: "ДоМ", translated_text: "дОм", review_date: Date.today)).not_to be_valid
+    expect(card_bad).not_to be_valid
   end
 end
