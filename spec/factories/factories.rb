a40a797b109a6db874a80bdfc05ad8a35bb9e495
zilon <a@vectordrop.com>
2015-04-03 00:06:14 +0300

    new card integration test

---
+++ spec/factories/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :card do
+    original_text "hausE SOmE ДоМ"
+    translated_text "hauses"
+  end
+end
---
+++ spec/features/new_cards_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "NewCards", :type => :feature do
+  card = FactoryGirl.create(:card)
+  describe "Post /new_cards" do
+    it "create card" do
+      visit new_card_path
+      fill_in "Original text", :with => card.original_text
+      fill_in "Translated text", :with => card.translated_text
+      click_button "Create Card"
+        expect(page).to have_content(I18n.t("card.created"))
+      expect(page).to have_content(card.original_text)
+    end
+  end
+end
