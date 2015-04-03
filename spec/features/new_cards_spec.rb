1b50bdb841f5905834b8b6ad885eda7723b12861
zilon <a@vectordrop.com>
2015-04-03 11:46:37 +0300

    fix specs

--- spec/features/new_cards_spec.rb
+++ spec/features/new_cards_spec.rb
@@ -1,15 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe "NewCards", :type => :feature do
+describe "NewCards" do
   card = FactoryGirl.create(:card)
-  describe "Post /new_cards" do
-    it "create card" do
-      visit new_card_path
-      fill_in "Original text", :with => card.original_text
-      fill_in "Translated text", :with => card.translated_text
-      click_button "Create Card"
-        expect(page).to have_content(I18n.t("card.created"))
-      expect(page).to have_content(card.original_text)
-    end
+  it "create card" do
+    visit new_card_path
+    fill_in "Original text", with: card.original_text
+    fill_in "Translated text", with: card.translated_text
+    click_button "Create Card"
+    expect(page).to have_content(I18n.t("card.created"))
+    expect(page).to have_content(card.original_text)
   end
 end
