6cb1e5a26ec1f6592889f8597448d9bc06df5ba4
zilon <a@vectordrop.com>
2015-04-10 12:22:22 +0300

    user specs

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,27 @@
+include UserHelper
+feature "user profile" do
+  let!(:user) { FactoryGirl.create(:user) }
+  before{
+    login
+  }
+  scenario "login" do
+    expect(page).to have_content("Nothing to review")
+  end
+  scenario "review" do
+    click_link "Edit Profile"
+    expect(page).to have_content("Editing user")
+  end
+  scenario "wrong password change" do
+    click_link "Edit Profile"
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: "some"
+    click_button "Update User"
+    expect(page).to have_content("errors")
+  end
+  scenario "correct password change" do
+    click_link "Edit Profile"
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password_confirmation
+    expect(page).to have_content("Editing user")
+  end
+end
