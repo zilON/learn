bbfb391828e87b9fcd6de6d21563c1cf057a08b0
zilon <a@vectordrop.com>
2015-04-10 10:00:15 +0300

    create user and cards

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :card do
+    user
     original_text "hausE SOmE ДоМ"
     translated_text "hauses"
-    user_id 1
     trait :to_check do
       review_date 3.days.ago
     end
