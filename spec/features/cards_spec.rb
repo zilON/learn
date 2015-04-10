3c9f1d702e394730bd8648f110f598017e017c43
zilon <a@vectordrop.com>
2015-04-10 12:05:02 +0300

    clean debug and spec crud to user cards

--- spec/features/cards_spec.rb
+++ spec/features/cards_spec.rb
@@ -2,7 +2,6 @@ include UserHelper
 feature "Review a card" do
   let!(:card) { FactoryGirl.create(:card, :to_check) }
   before{
-    visit root_path
     login
   }
   describe "review errors" do
@@ -11,17 +10,44 @@ feature "Review a card" do
       click_button "Update Card"
       expect(page).to have_content(I18n.t("card.review_error"))
     end
-
     scenario "shows error with translated_text used" do
       fill_in "Original text", with: card.translated_text
       click_button "Update Card"
       expect(page).to have_content(I18n.t("card.review_error"))
     end
   end
-
   scenario "shows sccess message and updates review_date" do
     fill_in "Original text", with: card.original_text
     click_button "Update Card"
     expect(page).to have_content(I18n.t("card.review_success"))
+  end
+
+end
+feature "Cards CRUD" do
+  let!(:card) { FactoryGirl.create(:card, :to_check) }
+  before{
+    login
+  }
+
+  scenario "create user card" do
+    visit new_card_path
+    fill_in "Original text", with: "Some original text"
+    fill_in "Translated text", with: "Some Translated text"
+    click_button "Create Card"
+    expect(page).to have_content("Some original text" "Some Translated text")
+  end
+
+  scenario "update user card" do
+    visit "cards/6"
+    click_link "Edit"
+    fill_in "Translated text", with: "Some diferent Translated text"
+    click_button "Update Card"
+    expect(page).to have_content("Some diferent Translated text")
+  end
+
+  scenario "delete user card" do
+    visit "cards/7"
+    click_link "Delete"
+    expect(page).to have_content(I18n.t("card.deleted"))
   end
 end
