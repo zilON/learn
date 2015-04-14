6c017474bda9e71512c02a1f40dfa9d74f6dc64b
zilon <a@vectordrop.com>
2015-04-14 19:53:05 +0300

    fix small bug

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,15 +5,15 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_back_or_to(:users, notice: "Login successful")
     else
-      flash.now[:alert] = 'Login failed'
+      flash.now[:alert] = "Login failed"
       render action: 'new'
     end
   end
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to(:users, notice: "Logged out!")
   end
 end
